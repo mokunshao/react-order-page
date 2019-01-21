1564143d62f278e14d36b942a034c308502c3624
mokunshao <1627510957@qq.com>
2019-01-21 23:49:49 +0800

    重新发布上线

---
+++ build/precache-manifest.60e7d7e25d5af90e06e00e765134835b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f62762487b347b709f2a",
+    "url": "/react-order-page/build/static/js/runtime~main.f6276248.js"
+  },
+  {
+    "revision": "034bf5f2e9331bf36907",
+    "url": "/react-order-page/build/static/js/main.034bf5f2.chunk.js"
+  },
+  {
+    "revision": "f9be30a4556287e8d5a5",
+    "url": "/react-order-page/build/static/js/1.f9be30a4.chunk.js"
+  },
+  {
+    "revision": "034bf5f2e9331bf36907",
+    "url": "/react-order-page/build/static/css/main.7671ca05.chunk.css"
+  },
+  {
+    "revision": "64d75c0cd5b6beb1a9f4171a0099611d",
+    "url": "/react-order-page/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17e41cc60943d142f6a60f01a60faf10.js"
+  "/react-order-page/build/precache-manifest.60e7d7e25d5af90e06e00e765134835b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-order-page/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
